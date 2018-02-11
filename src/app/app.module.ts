3d714df580729cc79d700ffe0dde4d7f4c731d76
Shailendra <shally@Shailendras-MacBook-Pro.local>
2018-02-11 14:20:06 +0530

    corrected ngmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AuthorsComponent } from './authors/authors.component';
@@ -13,7 +13,6 @@ import { AuthorsService } from './authors/authors.service';
   imports: [
     BrowserModule
   ],
-  schemas: [ CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
   providers: [AuthorsService],
   bootstrap: [AppComponent]
 })
